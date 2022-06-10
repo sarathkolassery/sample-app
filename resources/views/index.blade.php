d24684657320d177148845a417ff20c6f41c6494
sarath1995 <clicktosarath@gmail.com>
2022-06-10 13:47:49 +0530

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <body>
+        <div class="container">
+  <div class="row">
+  <div class="col-md-3">
+  </div>
+    <div class="col-md-6">
+      <h2>Enter your details</h2>
+      <div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">Name</span>
+  <input type="text" class="form-control" placeholder="Enter your name" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">Age</span>
+  <input type="text" class="form-control" placeholder="Enter your age" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">Place</span>
+  <input type="text" class="form-control" placeholder="Enter your place" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">Phone</span>
+  <input type="text" class="form-control" placeholder="Enter your phone number" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+
+    </div>
+  </div>
+</div>
+    </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</head>
+</html>
